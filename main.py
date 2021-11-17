784982f27487a8da95996c4a2232fd18dac0a477
Brendan <brendanelewis@gmail.com>
2021-11-17 12:49:37 -0500

    feat: set up weather api

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import requests
+from pprint import pprint
+import json
+
+key = ""
+try:
+    with open("secrets.json") as file:
+        key = json.load(file)["apiKey"]
+except FileNotFoundError:
+    print("""Please create a secrets.json file with the following format:
+
+{
+    "apiKey": "your-api-key-here"
+}
+""")
+    exit(1)
+
+base_url = "https://api.openweathermap.org/data/2.5/weather"
+city_id = input("Enter a city: ")
+weather_data = requests.get(base_url + "?q=" + city_id + "&appid=" + key).json()
+pprint(weather_data)
