096a526d917aad3e324344d91a7ec647e8616b88
Devin Cummings <dcummings1015@gmail.com>
2021-12-01 13:26:00 -0500

    Kelvin to celcius and celcius to farenheit added

--- main.py
+++ main.py
@@ -19,3 +19,9 @@ base_url = "https://api.openweathermap.org/data/2.5/weather"
 city_id = input("Enter a city: ")
 weather_data = requests.get(base_url + "?q=" + city_id + "&appid=" + key).json()
 pprint(weather_data)
+
+def kelvin_to_celcius(degrees: int)->int:
+    return degrees-273
+
+def celcius_to_farenheit(degrees: int)->int:
+    return (degrees*1.8)+32
