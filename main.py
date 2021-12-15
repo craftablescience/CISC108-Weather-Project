3db903e6011d71bf7ac3a24f953096fcbc2d0596
emmaframpton <emmalynneframpton@gmail.com>
2021-12-15 17:07:31 -0500

    feat: added if_cloudy function

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from cisc108 import assert_equal
 from openweathermap import *
 
-print(make_request(q="Boise"))
+print(make_request(q="Sudan"))
 
 def kelvin_to_celsius(degrees: float)->float:
     '''
@@ -67,6 +67,19 @@ assert_equal(if_raining({'coord': {'lon': -79.9959, 'lat': 40.4406}, 'weather': 
 assert_equal(if_raining({'coord': {'lon': -74.1724, 'lat': 40.7357}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}], 'base': 'stations', 'main': {'temp': 282.23, 'feels_like': 282.23, 'temp_min': 280.43, 'temp_max': 283.89, 'pressure': 1020, 'humidity': 47}, 'visibility': 10000, 'wind': {'speed': 0.89, 'deg': 179, 'gust': 2.68}, 'clouds': {'all': 1}, 'dt': 1638388045, 'sys': {'type': 2, 'id': 2003689, 'country': 'US', 'sunrise': 1638360113, 'sunset': 1638394211}, 'timezone': -18000, 'id': 5101798, 'name': 'Newark', 'cod': 200}
 ), False)
 
+def if_cloudy(weather_set: dict) -> float:
+    '''
+
+    :param weather_set:
+    :return:
+    '''
+    return weather_set["clouds"]["all"] > 50
+assert_equal(if_cloudy({'coord': {'lon': 140.2167, 'lat': 39.65}, 'weather': [{'id': 600, 'main': 'Snow', 'description': 'light snow', 'icon': '13n'}], 'base': 'stations', 'main': {'temp': 274.67, 'feels_like': 267.86, 'temp_min': 274.67, 'temp_max': 274.67, 'pressure': 1008, 'humidity': 74, 'sea_level': 1008, 'grnd_level': 1005}, 'visibility': 2667, 'wind': {'speed': 10.92, 'deg': 285, 'gust': 17.16}, 'snow': {'1h': 0.22}, 'clouds': {'all': 100}, 'dt': 1638388040, 'sys': {'country': 'JP', 'sunrise': 1638394899, 'sunset': 1638429336}, 'timezone': 32400, 'id': 2110608, 'name': 'Wada', 'cod': 200}
+), True)
+assert_equal(if_cloudy({'coord': {'lon': -79.9959, 'lat': 40.4406}, 'weather': [{'id': 501, 'main': 'Rain', 'description': 'moderate rain', 'icon': '10d'}, {'id': 701, 'main': 'Mist', 'description': 'mist', 'icon': '50d'}], 'base': 'stations', 'main': {'temp': 284.68, 'feels_like': 284.29, 'temp_min': 282.68, 'temp_max': 286.42, 'pressure': 1005, 'humidity': 92}, 'visibility': 8047, 'wind': {'speed': 0.89, 'deg': 233, 'gust': 7.6}, 'rain': {'1h': 1.62}, 'clouds': {'all': 90}, 'dt': 1638812611, 'sys': {'type': 2, 'id': 2008550, 'country': 'US', 'sunrise': 1638793749, 'sunset': 1638827610}, 'timezone': -18000, 'id': 5206379, 'name': 'Pittsburgh', 'cod': 200}
+), True)
+assert_equal(if_cloudy({'coord': {'lon': -74.1724, 'lat': 40.7357}, 'weather': [{'id': 800, 'main': 'Clear', 'description': 'clear sky', 'icon': '01d'}], 'base': 'stations', 'main': {'temp': 282.23, 'feels_like': 282.23, 'temp_min': 280.43, 'temp_max': 283.89, 'pressure': 1020, 'humidity': 47}, 'visibility': 10000, 'wind': {'speed': 0.89, 'deg': 179, 'gust': 2.68}, 'clouds': {'all': 1}, 'dt': 1638388045, 'sys': {'type': 2, 'id': 2003689, 'country': 'US', 'sunrise': 1638360113, 'sunset': 1638394211}, 'timezone': -18000, 'id': 5101798, 'name': 'Newark', 'cod': 200}
+), False)
 
 def clothes_by_temp(degrees: float)-> str:
     '''
@@ -140,7 +153,4 @@ def clothes_for_location(location: str) -> list:
         return ["raincoat", "long sleeve", "pants", "boots", "umbrella"]
     else:
         return clothes_by_temp(celsius_to_fahrenheit(kelvin_to_celsius(feels_like_temperature(find_weather_location(location)))))
-assert_equal(clothes_for_location("Newark"), ["jacket", "sweatshirt", "long sleeve", "pants", "sneakers"])
-assert_equal(clothes_for_location("Canberra"), ["raincoat", "long sleeve", "pants", "boots", "umbrella"])
-assert_equal(clothes_for_location("Salt Lake City"), ["hat", "heavy coat", "scarf", "long sleeve", "pants", "boots"])
-assert_equal(clothes_for_location("Mexico City"), ["t-shirt", "pants", "sneakers"])
+
